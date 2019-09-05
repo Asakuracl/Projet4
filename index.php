e30c7476610e635de8cc9b8326568b917386b350
asakuracl <champi.ludo@hotmail.fr>
2019-09-05 10:47:31 -0300

    add bootstrap

--- index.php
+++ index.php
@@ -5,33 +5,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <title>Document</title>
 </head>
 <body>
-    <header>
-        <h1>Billet simple pour l'Alaska</h1>
-    </header>
-    
-    <section>
-        <?php
-        $db = new PDO('mysql:host=localhost;dbname=projet;charset=utf8', 'root', 'AsakuraCl+4', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-        $req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
+    <div class="container">
+        <header class="row">
+            <div class=col-lg-12>
+                <h1>Billet simple pour l'Alaska</h1>
+            </div>
+        </header>
         
-        while($dataPosts= $req->fetch())
-        {
-        ?>
-        <div class="news">
-        <a class="link-content" href=comment.php?post=<?php echo $dataPosts['id']
-        ?></a>
-            <h3>
-                <?php echo htmlspecialchars($dataPosts['title']); ?>
-                <br>le <?php echo $dataPosts['creation_date_fr']; ?>
-            </h3>
-        </div>
-    </section>
-        <?php
-        }
-        $req->closeCursor();
-        ?>
+        <section>
+            <?php
+            $db = new PDO('mysql:host=localhost;dbname=projet;charset=utf8', 'root', 'AsakuraCl+4', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            $req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
+            
+            while($dataPosts= $req->fetch())
+            {
+            ?>
+            <div class="news">
+            <a class="link-content" href=comment.php?post=<?php echo $dataPosts['id']
+            ?></a>
+                <h3>
+                    <?php echo htmlspecialchars($dataPosts['title']); ?>
+                    <br>le <?php echo $dataPosts['creation_date_fr']; ?>
+                </h3>
+            </div>
+        </section>
+            <?php
+            }
+            $req->closeCursor();
+            ?>
+    </div>
+<!-- script -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
 </body>
 </html>
