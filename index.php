d45ded343b5bbbf3af4029880dcb985e302e8632
asakuracl <champi.ludo@hotmail.fr>
2019-09-08 19:56:58 -0300

    get model, call getPosts & get view

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-    $db = new PDO('mysql:host=localhost;dbname=projet;charset=utf8', 'root', 'AsakuraCl+4', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    require('model.php');
 
-    $req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
+    $req = getPosts();
 
     require('view.php');
 ?>
