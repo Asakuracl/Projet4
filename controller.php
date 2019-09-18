192b93f057435143f099556d6d55b76dd7fedff5
asakuracl <champi.ludo@hotmail.fr>
2019-09-17 22:46:25 -0300

    missing require postView

--- controller.php
+++ controller.php
@@ -10,15 +10,6 @@
     function post(){
         $post = getPost($_GET['id']);
         $comments = getComments($_GET['id']);
-    }
 
-    //
-    /*
-    if (isset($_GET['id']) && $_GET['id'] > 0){
-        $post = getPost($_GET['id']);
-        $comments = getComments($_GET['id']);
         require('postView.php');
-    } else {
-        echo 'Erreur : aucun identifiant de billet envoyé';
     }
-   */
