7846de17267ba4d366a1b54bde21e4cf16bc09e5
asakuracl <champi.ludo@hotmail.fr>
2019-09-17 22:13:36 -0300

    add condition for each controller

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <?php
-    require('controller.php');
+require('controller.php');
 
+    if (isset($_GET['action'])) {
+        if ($_GET['action'] == 'listPosts'){
+            lisPosts();
+        } elseif ($_GET['action'] == 'post'){
+            if (isset($_GET['id']) && $_GET['id'] > 0){
+                post();
+        } else {
+        echo 'Erreur : aucun identifiant de billet envoyé';
+        }
 
+        }
+    } else {
+        listPosts(); 
+    }
     //
     /*
     $posts = getPosts();
