0cf03700753b5ca1239e3912efbc506db12b68b1
asakuracl <champi.ludo@hotmail.fr>
2019-10-06 00:57:12 -0300

    section row & div out of while, update indent

--- view/frontend/postView.php
+++ view/frontend/postView.php
@@ -9,53 +9,55 @@
     </header>
     
     <section class="row post">
-            <div class="col-6 offset-3">
-                <h3>
-                    <?= htmlspecialchars($post['title']); ?>
-                </h3>
-                
-                <p>
-                    <?= nl2br(htmlspecialchars($post['content'])); ?>
-                </p>
+        <div class="col-6 offset-3">
+            <h3>
+                <?= htmlspecialchars($post['title']); ?>
+            </h3>
+            
+            <p>
+                <?= nl2br(htmlspecialchars($post['content'])); ?>
+            </p>
 
-                <h2>Commentaires</h2>
+            <h2>Commentaires</h2>
 
-                <form action="index.php?action=addComment&id=<?php echo $post['id'] ?>" method="post">
-                    <div>
-                        <label for="author">Auteur</label><br>
-                        <input type="text" id="author" name="author" />
-                    </div>
-                    <div>
-                        <label for="comment">Commentaire</label><br>
-                        <textarea id="comment" name="comment"></textarea>
-                    </div>
-                    <div>
-                        <input type="submit" />
-                    </div>
-                </form>
-            </div>
-
-                <p class="col-6 offset-3"><a href="index.php">Retour à la liste des chapitres</a>
-                </p>
-    </section>
-        <?php
-        while($comment = $comments->fetch())
-        {
-        ?>
-            <section class="row comment">
-                <div class="col-6 offset-3">
-                    <p>
-                        <?= htmlspecialchars($comment['author'].':'); ?>
-                        le <?= $comment['comment_date_fr']; ?>
-                    </p>
-                    <p>
-                        <?= nl2br(htmlspecialchars($comment['comment'])); ?>
-                    </p>
-                        <?php
-                        }
-                        ?>
+            <form action="index.php?action=addComment&id=<?php echo $post['id'] ?>" method="post">
+                <div>
+                    <label for="author">Auteur</label><br>
+                    <input type="text" id="author" name="author" />
                 </div>
-            </section>
+                <div>
+                    <label for="comment">Commentaire</label><br>
+                    <textarea id="comment" name="comment"></textarea>
+                </div>
+                <div>
+                    <input type="submit" />
+                </div>
+            </form>
+        </div>
+
+            <p class="col-6 offset-3"><a href="index.php">Retour à la liste des chapitres</a>
+            </p>
+    </section>
+    <section class="row comment">
+        <div class="col-6 offset-3">
+            <?php
+            while($comment = $comments->fetch())
+            {
+            ?>
+                <!--<section class="row comment">
+                    <div class="col-6 offset-3">-->
+                <p>
+                    <?= htmlspecialchars($comment['author'].':'); ?>
+                    le <?= $comment['comment_date_fr']; ?>
+                </p>
+                <p>
+                    <?= nl2br(htmlspecialchars($comment['comment'])); ?>
+                </p>
+            <?php
+            }
+            ?>
+        </div>
+    </section>
 <?php $content = ob_get_clean() ?>
 
 <?php require("template.php"); ?>
