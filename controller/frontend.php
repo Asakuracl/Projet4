1242f2740dd459d25bd6c009e1eadcb5cf574b6e
asakuracl <champi.ludo@hotmail.fr>
2019-10-07 19:54:25 -0300

    update to require once the class

--- controller/frontend.php
+++ controller/frontend.php
@@ -1,25 +1,26 @@
 <?php
-    require('model\frontend.php');
+require_once('model\PostManager.php');
+require_once('model\CommentManager.php');
 
-    function listPosts(){
-        $posts = getPosts();
+function listPosts(){
+    $posts = getPosts();
 
-        require('view\frontend\view.php');
+    require('view\frontend\view.php');
+}
+
+function post(){
+    $post = getPost($_GET['id']);
+    $comments = getComments($_GET['id']);
+
+    require('view\frontend\postView.php');
+}
+
+function addComment($postId, $author, $comment){
+    $addComment = postComment($postId, $author, $comment);
+
+    if ($addComment === false){
+        die("Erreur d'ajout du commentaire");
+    } else{
+        header("Location: index.php?action=post&id=" . $postId);
     }
-
-    function post(){
-        $post = getPost($_GET['id']);
-        $comments = getComments($_GET['id']);
-
-        require('view\frontend\postView.php');
-    }
-
-    function addComment($postId, $author, $comment){
-        $addComment = postComment($postId, $author, $comment);
-
-        if ($addComment === false){
-            die("Erreur d'ajout du commentaire");
-        } else{
-            header("Location: index.php?action=post&id=" . $postId);
-        }
-    }
+}
