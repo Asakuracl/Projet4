b3ea4808b89f36302d35452ae2fe29f122a8b266
asakuracl <champi.ludo@hotmail.fr>
2019-10-07 19:37:24 -0300

    add class and insert public/private to function

--- model/PostManager.dist.php
+++ model/PostManager.dist.php
@@ -1,41 +1,43 @@
 // to replace model
 
 <?php
-function getPosts(){
-    $db =  dbConnect();
-    $req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
+class PostManager{
+    public function getPosts(){
+        $db =  dbConnect();
+        $req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
 
-    return $req;
-}
+        return $req;
+    }
 
-function getPost($postId){
-    $db =  dbConnect();
-    $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin\') AS creation_date_fr FROM posts WHERE id=?');
-    $req->execute(array($postId));
-    $post = $req->fetch();
+    public function getPost($postId){
+        $db =  dbConnect();
+        $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin\') AS creation_date_fr FROM posts WHERE id=?');
+        $req->execute(array($postId));
+        $post = $req->fetch();
 
-    return $post;
-}
+        return $post;
+    }
 
-function getComments($postId){
-    $db =  dbConnect();
-    $comments = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin\') AS comment_date_fr FROM comments WHERE post_id=? ORDER BY comment_date DESC');
-    $comments->execute(array($postId));
+    public function getComments($postId){
+        $db =  dbConnect();
+        $comments = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin\') AS comment_date_fr FROM comments WHERE post_id=? ORDER BY comment_date DESC');
+        $comments->execute(array($postId));
 
-    return $comments;
-}
+        return $comments;
+    }
 
-function postComment($postId, $author, $comment){
-    $db = dbConnect();
+    public function postComment($postId, $author, $comment){
+        $db = dbConnect();
 
-    $comments = $db->prepare('INSERT INTO comments (post_id, author, comment, comment_date) VALUES(?, ?, ?, Now())');
-    $addComment = $comments->execute(array($postId, $author, $comment));
+        $comments = $db->prepare('INSERT INTO comments (post_id, author, comment, comment_date) VALUES(?, ?, ?, Now())');
+        $addComment = $comments->execute(array($postId, $author, $comment));
 
-    return $addComment;
-}
+        return $addComment;
+    }
 
-function dbConnect(){
-     $db = new PDO('mysql:host=localhost;dbname=projet;charset=utf8', '***', '***');
+    private function dbConnect(){
+        $db = new PDO('mysql:host=localhost;dbname=projet;charset=utf8', '***', '***');
 
-      return $db;
+        return $db;
+    }
 }
