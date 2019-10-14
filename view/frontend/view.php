a96ae130dae944026a2dc75a4c0d2ad9fc466793
asakuracl <champi.ludo@hotmail.fr>
2019-10-13 21:39:54 -0300

    remove the last div

--- view/frontend/view.php
+++ view/frontend/view.php
@@ -2,47 +2,47 @@
 
 <?php ob_start() ?>
 
-        <div class="row">
-            <h1 class="col-6 offset-3">Billet simple pour l'Alaska</h1>
-        </div>
-    </header>
-    
-    <section class="first-section row">
-        <?php
-        while($dataPosts= $posts->fetch())
-        {
-        ?>
-        <div class="news col-lg-4 rounded  border">
-            <a class="link-comment" href="index.php?action=post&amp;id=<?= $dataPosts['id']; ?>">
-                <h3>
-                    <?= htmlspecialchars($dataPosts['title']); ?>
-                </h3>
-            </a>
-            <p>
-                <br>le <?= $dataPosts['creation_date_fr']; ?>
-            </p>
-        </div>
-        <?php
-        }
-        $posts->closeCursor();
-        ?>
-            <!-- zone de test -->
-        <form action="index.php?action=addPost" method="post">
-            <div>
-                <label for="title">Titre</label><br>
-                <input type="text" id="title" name="title" />
-            </div>
-            <div>
-                <label for="content">Contenu</label><br>
-                <textarea id="content" name="content"></textarea>
-            </div>
-            <div>
-                <input type="submit" />
-            </div>
-        </form>
+    <div class="row">
+        <h1 class="col-6 offset-3">Billet simple pour l'Alaska</h1>
+    </div>
+</header>
+
+<section class="first-section row">
+    <?php
+    while($dataPosts= $posts->fetch())
+    {
+    ?>
+    <div class="news col-lg-4 rounded  border">
+        <a class="link-comment" href="index.php?action=post&amp;id=<?= $dataPosts['id']; ?>">
+            <h3>
+                <?= htmlspecialchars($dataPosts['title']); ?>
+            </h3>
+        </a>
+        <p>
+            <br>le <?= $dataPosts['creation_date_fr']; ?>
+        </p>
+    </div>
+    <?php
+    }
+    $posts->closeCursor();
+    ?>
         <!-- zone de test -->
-    </section>
-</div>
+    <form action="index.php?action=addPost" method="post">
+        <div>
+            <label for="title">Titre</label><br>
+            <input type="text" id="title" name="title" />
+        </div>
+        <div>
+            <label for="content">Contenu</label><br>
+            <textarea id="content" name="content"></textarea>
+        </div>
+        <div>
+            <input type="submit" />
+        </div>
+    </form>
+    <!-- zone de test -->
+</section>
+
 <?php $content = ob_get_clean() ?>
 
 <?php require("template.php"); ?>
