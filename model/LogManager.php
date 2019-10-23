769b5ae5ea7d023af60776f4da7f03793b6a5629
asakuracl <champi.ludo@hotmail.fr>
2019-10-22 22:39:01 -0300

    clean space

--- model/LogManager.php
+++ model/LogManager.php
@@ -4,16 +4,6 @@ require_once('Manager.php');
 
 class LogManager extends Manager{
 
-   /*
-    public function postComment($postId, $author, $comment){
-        $db = $this->dbConnect();
-
-        $comments = $db->prepare('INSERT INTO comments (post_id, author, comment, comment_date) VALUES(?, ?, ?, Now())');
-        $addComment = $comments->execute(array($postId, $author, $comment));
-
-        return $addComment;
-    }
-    */
     // login
     public function logIn($nickname){
         $db =  $this->dbConnect();
