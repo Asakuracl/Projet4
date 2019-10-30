e55bc79796ef12fa3a2c325b390e03475e9d87e5
asakuracl <champi.ludo@hotmail.fr>
2019-10-30 17:33:26 -0300

    add in view, login form

--- view/frontend/login.php
+++ view/frontend/login.php
@@ -15,6 +15,18 @@
             </div>
             <button type="submit" class="button-login"> Se connecter</button>
     </form>
+
+    <!-- checklog--> 
+    <form class="col-6 offset-3" action="/projet4/index.php?action=checkLog" method="post">
+        <p>Formulaire de connexion :</p>
+            <div class="form-group">
+                <input class="form-control" type="text" name="nickname" placeholder="nom d'utilisateur">
+            </div>
+            <div class="form-group">
+                <input class="form-control" type="password" name="pass" placeholder="mot de passe">
+            </div>
+            <button type="submit" class="button-login"> Se connecter</button>
+    </form>
 </section>
 <?php $content = ob_get_clean(); ?>
 
