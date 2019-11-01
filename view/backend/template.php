3d9d88e48393920bd187d414d92e73639e6047c0
asakuracl <champi.ludo@hotmail.fr>
2019-11-01 20:43:11 -0300

    creat backend template

---
+++ view/backend/template.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="public/css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <title><?= $title ?></title>
+</head>
+<body class="main-body">
+    <div class="container">
+
+<header>
+    <div class="row">
+        <p class="col">Admin</p>
+        <ul class="nav">
+            <li>
+                <a class="nav-link">
+                    <?php
+                        if (isset($_SESSION['id']) AND isset($_SESSION['nickname'])){
+                            echo "Bonjour ". $_SESSION['nickname'];
+                        }
+                    ?>
+                </a>
+            </li> 
+            <li>
+                <a class="nav-link" href="/projet4/index.php">Accueil</a>
+            </li>
+            <li>
+                <?php
+                    if (isset($_SESSION['id']) AND isset($_SESSION['nickname'])){
+                ?>
+                    <a class="nav-link" href="/projet4/view/frontend/logout.php">Se déconnecter</a>
+                <?php
+                    } else {
+                ?>
+                    <a class="nav-link" href="/projet4/view/frontend/login.php">Se connecter</a>
+                <?php
+                    } 
+                ?>
+            </li>              
+        </ul>
+    </div>
+
+<?= $content ?>
+</div>
+<!-- script -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+    
+</html>
