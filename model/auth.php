28164555292360f770f2b6c780f94a2437e6e09d
asakuracl <champi.ludo@hotmail.fr>
2019-11-03 09:04:19 -0300

    comment

--- model/auth.php
+++ model/auth.php
@@ -1,7 +1,11 @@
 <?php 
+
+/*
 function connected () {
-    if (session_status()=== PHP_SESSION_NONE){
+    if (session_status() === PHP_SESSION_NONE){
         session_start();
     }
     return !empty($_SESSION['connected']);
 }
+
+*/
