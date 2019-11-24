7af1aa051ff4bcde8fa53cd07d7154159c882096
asakuracl <champi.ludo@hotmail.fr>
2019-11-24 00:05:51 -0300

    creat a for adminview

--- view/backend/adminView.php
+++ view/backend/adminView.php
@@ -12,7 +12,29 @@ if (isset($_SESSION)){
 </header>
 
 <section class="first-section row">
-  
+    <div class="news col-md-4 rounded  border">
+        <a class="link-comment" href="#">
+            <h3 class="offset-3">
+                Creer un chapitre
+            </h3>
+        </a>   
+    </div>
+
+    <div class="news col-lg-4 rounded  border">
+        <a class="link-comment" href="#">
+            <h3 class="offset-1">
+                mise à jour de billets
+            </h3>
+        </a>
+    </div>
+
+    <div class="news col-lg-4 rounded  border">
+        <a class="link-comment" href="#">
+            <h3 class="offset-3">
+                suppression de billets
+            </h3>
+        </a>
+    </div>
 </section>
 
 <?php
