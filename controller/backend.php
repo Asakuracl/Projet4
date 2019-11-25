5232feb279c90b08b0b12b3d035b56a083b2cc32
asakuracl <champi.ludo@hotmail.fr>
2019-11-24 21:24:11 -0300

    insert addmenber function

--- controller/backend.php
+++ controller/backend.php
@@ -2,3 +2,20 @@
 
 require_once('model\PostManager.php');
 require_once('model\CommentManager.php');
+
+//addmenber
+function addMember($nickname, $pass){
+    $memberManager = new LogManager();
+    $pass_hash = password_hash($pass, PASSWORD_DEFAULT);
+    $addMember = $memberManager->createMember($nickname, $pass_hash);
+
+    //trying have a succes message, if add ok
+    $success = null;
+
+    if ($addMember === true){
+        header("Location: view/frontend/login.php");
+        $success = "Membre ajouté !";     
+    } else{
+        die("Erreur ajout du membre"); 
+    }
+}
