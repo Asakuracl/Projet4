465b400793586d17812285c77c30ccdcacd9a425
asakuracl <champi.ludo@hotmail.fr>
2019-12-11 20:07:28 -0300

    add charter graphic : center h1, bg-dark, text-light, text-dark. Add section for ul.

--- view/backend/adminView.php
+++ view/backend/adminView.php
@@ -5,30 +5,29 @@
 if (isset($_SESSION['nickname'])){
 
 ?>
-
-    <div class="row">
-        <h1 class="offset-3 col-6">Selectionner votre action</h1>
+    <div class="row bg-dark text-light rounded">
+        <h1 class="mx-auto px-5 my-1">Selectionner votre action</h1>
     </div>
 </header>
 
-
-<ul class="nav">
-    <li class="nav-item">
-        <h3>
-            <a class="nav-link" href="index.php?action=createPost">
-                Creer un chapitre
-            </a>
-        </h3>
-    </li>
-    <li class="nav-item">
-        <h3>
-            <a class="nav-link" href="index.php?action=erasePost">
-                administrer les billets
-            </a>
-        </h3>
-    </li>
-</ul>
-
+<section>
+    <ul class="nav flex-column">
+        <li class="nav-item">
+            <h3>
+                <a class="nav-link text-dark" href="index.php?action=createPost">
+                    Creer un chapitre
+                </a>
+            </h3>
+        </li>
+        <li class="nav-item">
+            <h3>
+                <a class="nav-link text-dark" href="index.php?action=erasePost">
+                    administrer les billets
+                </a>
+            </h3>
+        </li>
+    </ul>
+</section>
 
 <?php
 }
