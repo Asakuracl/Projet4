0a1e4fa9c20bc4b6d04a1849bea6c4a0c6a6266c
asakuracl <champi.ludo@hotmail.fr>
2019-12-10 23:06:04 -0300

    add a graphic charter. text-light, bg-dark, text-black-50, bg-white. Customise form

--- view/frontend/postView.php
+++ view/frontend/postView.php
@@ -3,38 +3,40 @@
 <?php ob_start() ?>
 
         <div class=row>
-            <h1 class=col>Billet simple pour l'Alaska</h1>
+            <h1 class="col bg-dark text-light rounded">Billet simple pour l'Alaska</h1>
         </div>
     </header>
 
 <section class="row post">
     <div class="col-6 offset-3">
         <h3>
-            <?= htmlspecialchars($post['title']); ?>
+            <u>
+                <?= htmlspecialchars($post['title']); ?>
+            </u>
         </h3>
         
         <p>
             <?= nl2br(htmlspecialchars($post['content'])); ?>
         </p>
 
-        <h2>Commentaires</h2>
+        <h2 class="my-3">Commentaires</h2>
 
         <form action="index.php?action=addComment&amp;id=<?php echo $post['id'] ?>" method="post">
-            <div>
-                <label for="author">Auteur</label><br>
-                <input type="text" id="author" name="author" />
+            <div class="form-group">
+                <label for="author">Auteur</label>
+                <input type="text" id="author" class="bg-light form-control" name="author" placeholder="Auteur"/>
+            </div>
+            <div class="form-group">
+                <label for="comment">Commentaire</label>
+                <textarea id="comment" class="bg-light form-control" name="comment"></textarea>
             </div>
             <div>
-                <label for="comment">Commentaire</label><br>
-                <textarea id="comment" name="comment"></textarea>
-            </div>
-            <div>
-                <input type="submit" />
+                <input type="submit" class="btn btn-dark mb-3"/>
             </div>
         </form>
     </div>
 
-        <p class="col-6 offset-3"><a href="index.php">Retour à la liste des chapitres</a>
+        <p class="col-6 offset-3"><a href="index.php" class="text-dark">Retour à la liste des chapitres</a>
         </p>
 </section>
 <section class="row">
@@ -45,18 +47,18 @@
         ?>
             <p class="header-comment">
                 <span class="text-secondary">
-                    <i class="fas fa-circle"></i> le <?= $comment['comment_date_fr'].' par '; ?>
+                    <i class="fas fa-circle text-warning border rounded border-dark"></i> le <?= $comment['comment_date_fr'].' par '; ?>
                 </span>
-                <span class="text-success">
+                <span class="text-white bg-dark px-1 rounded">
                     <?= htmlspecialchars($comment['author']); ?>
                 </span>
                 <a href="index.php?action=warningComment&amp;id=<?= $comment['id']; ?>">
-                    <button type="submit" id="alarm" class="btn btn-outline-danger">
+                    <button type="submit" id="alarm" class="btn btn-outline-warning">
                         <i class="fas fa-bullhorn"></i>
                     </button>
                 </a>
             </p>
-            <p class="content-comment">
+            <p class="content-comment text-dark bg-light border-bottom border-secondary ml-4 px-2 pb-4">
                 <?= nl2br(htmlspecialchars($comment['comment'])); ?>
             </p>
         <?php
