2e07314aa69bb7bece0f6b7887ea604735d8f918
asakuracl <champi.ludo@hotmail.fr>
2019-12-30 23:09:59 -0300

    add navbar burger menu. Add footer & jquery script

--- view/backend/template.php
+++ view/backend/template.php
@@ -14,35 +14,75 @@
 
 <body class="main-body container">
     <header>
-        <div class="row">
-            <p class="col">Admin</p>
-            <ul class="nav">
-                <li>
-                    <a class="nav-link text-dark">
-                        <?php
+        <div>
+            <p>Admin</p>
+            <nav class="navbar navbar-expand-lg navbar-light bg-white py-0 my-3">
+                <button class=" navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li>
+                            <a class="nav-link text-dark">
+                                <?php
                             if (isset($_SESSION['nickname'])) {
                                 echo "Bonjour " . $_SESSION['nickname'];
                             }
                         ?>
-                    </a>
-                </li>
-                <li>
-                    <a class="nav-link text-dark underline-effect" href="/projet4/index.php">Accueil</a>
-                </li>
-                <li>
-                    <a class="nav-link text-dark underline-effect" href="/projet4/index.php?action=adminPage">Accueil
-                        Administration</a>
-                </li>
-                <li>
-                    <a class="nav-link text-dark underline-effect" href="/projet4/view/frontend/logout.php">Se
-                        déconnecter</a>
-                </li>
-            </ul>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="nav-link text-dark underline-effect" href="/projet4/index.php">Accueil</a>
+                        </li>
+                        <li>
+                            <a class="nav-link text-dark underline-effect"
+                                href="/projet4/index.php?action=adminPage">Accueil
+                                Administration</a>
+                        </li>
+                        <li>
+                            <a class="nav-link text-dark underline-effect" href="/projet4/view/frontend/logout.php">Se
+                                déconnecter</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </div>
 
         <?=$content?>
 
+        <footer class="border-top border-dark mt-5 text-right">
+            <p>
+                <img src="public\images\miniLivre2.jpg" class="rounded-top" />
+                Jean Forteroche, prenez votre plume !
+                <a href="https://twitter.com/neographe_org" target="_blank" class="text-dark text-decoration-none"
+                    style="font-size: 25px;">
+                    <i class="fab fa-twitter"></i>
+                </a>
+                <a href="https://www.instagram.com/asakura.cl/?hl=fr" target="_blank"
+                    class="text-dark text-decoration-none" style="font-size: 25px;">
+                    <i class="fab fa-instagram"></i>
+                </a>
+            </p>
+            <p>
+                <a href="http://neographe.org/" class="text-dark text-decoration-none mr-1">
+                    <i class="fas fa-circle text-warning border rounded border-dark"></i>À propos
+                </a>
+                <a href="http://neographe.org/" class="text-dark text-decoration-none mr-1">
+                    <i class="fas fa-circle text-warning border rounded border-dark"></i>Mentions légales
+                </a>
+                <a href="http://neographe.org/" class="text-dark text-decoration-none mr-1">
+                    <i class="fas fa-circle text-warning border rounded border-dark"></i>Contact
+                </a>
+            </p>
+            <p> Projet 4 formation OC </p>
+        </footer>
         <!-- script -->
+        <script src=" https://code.jquery.com/jquery-3.4.1.min.js"
+            integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous">
+        </script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
         <script
             src="https://cdn.tiny.cloud/1/xcs5ihxcsh4gaznurwzojymkulh3h9cd8evxurucg20opu9z/tinymce/5/tinymce.min.js">
         </script>
@@ -51,8 +91,6 @@
             selector: 'textarea'
         });
         </script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-
 </body>
 
 </html>
