ba56c01e02ac1f3156753237eaeb31c21a038cf2
asakuracl <champi.ludo@hotmail.fr>
2020-01-01 18:41:32 -0300

    remove part oif header. get div h1 in section. update padding

--- view/frontend/view.php
+++ view/frontend/view.php
@@ -2,29 +2,29 @@
 
 <?php ob_start()?>
 
-<div class="row bg-dark text-light rounded">
-    <h1 class="mx-auto">Billet simple pour l'Alaska</h1>
-</div>
-</header>
-
-<section class="first-section backgroundImage row">
-    <?php
-        while ($dataPosts = $posts->fetch()) {
-    ?>
-    <div class="news col-lg-3 rounded bg-white">
-        <a class="text-dark text-decoration-none" href="index.php?action=post&amp;id=<?=$dataPosts['id'];?>">
-            <h2>
-                <?=htmlspecialchars($dataPosts['title']);?>
-            </h2>
-        </a>
-        <p class="text-black-50">
-            <br>le <?=$dataPosts['creation_date_fr'];?>
-        </p>
+<section class="first-section pt-5">
+    <div class="row bg-dark text-light rounded">
+        <h1 class="mx-auto">Billet simple pour l'Alaska</h1>
     </div>
-    <?php
+    <div class="row backgroundImage">
+        <?php
+        while ($dataPosts = $posts->fetch()) {
+        ?>
+        <div class="news col-lg-3 rounded bg-white">
+            <a class="text-dark text-decoration-none" href="index.php?action=post&amp;id=<?=$dataPosts['id'];?>">
+                <h2>
+                    <?=htmlspecialchars($dataPosts['title']);?>
+                </h2>
+            </a>
+            <p class="text-black-50">
+                <br>le <?=$dataPosts['creation_date_fr'];?>
+            </p>
+        </div>
+        <?php
         }
         $posts->closeCursor();
     ?>
+    </div>
 </section>
 
 <?php $content = ob_get_clean()?>
