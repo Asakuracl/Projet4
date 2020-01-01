f920f8fe7951dc3502b7d36308c118233382a66c
asakuracl <champi.ludo@hotmail.fr>
2020-01-01 18:51:19 -0300

    remove part of header. put div h1 in section.

--- view/backend/adminView.php
+++ view/backend/adminView.php
@@ -5,12 +5,11 @@
 
     if (isset($_SESSION['nickname'])) {
 ?>
-<div class="row bg-dark text-light rounded">
-    <h1 class="mx-auto px-5 my-1">Selectionner votre action</h1>
-</div>
-</header>
 
-<section>
+<section class="pt-5">
+    <div class="row bg-dark text-light rounded">
+        <h1 class="mx-auto px-5 my-1">Selectionner votre action</h1>
+    </div>
     <ul class="nav flex-column">
         <li class="nav-item">
             <h2>
